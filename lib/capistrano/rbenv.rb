7455809d197e43d24d8bd117f3c05de70a5b850e
Kir Shatrov <shatrov@me.com>
2013-07-31 22:34:07 +0300

    Hack SSHKit

--- lib/capistrano/rbenv.rb
+++ lib/capistrano/rbenv.rb
@@ -1,4 +1,6 @@
 require 'capistrano/dsl/rbenv'
 self.extend Capistrano::DSL::Rbenv
 
+SSHKit::Backend::Netssh.send(:include, Capistrano::DSL::Rbenv)
+
 load File.expand_path("../tasks/rbenv.rake", __FILE__)
